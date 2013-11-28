a87db9d45165dc49e2315e3dad34f17e42e9cd5e
Steven Dwy <yoplitein+git@gmail.com>
2013-11-28 12:43:12 -0800

    Better sus

--- handlers/sus.py
+++ handlers/sus.py
@@ -1,9 +1,28 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
-"""
-A simple sample module with Skype smileys
-"""
+import os
+import sys
+import random
+import time
+
+random.seed(time.time())
+
+word = "sus"
+who = None
+
+if len(sys.argv) > 1:
+    if any([sys.argv[1].startswith(x) for x in ['"', "'"]]):
+        word = sys.argv[1][1:][:-1]
+        
+        sys.argv.pop(1)
+    
+    if len(sys.argv) >1:
+        who = " ".join(sys.argv[1:])
+
+if not who:
+    who = os.environ["NAME"]
+
+msg = "%s %s" % (word, who,)
 
 for x in range(0, 5):
-    print (" "*x) + "sus"
+    print (" "*random.randrange(0, 15)) + msg
 
