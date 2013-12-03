786e5ae6e6a9704bd2e08085afe44e3d7a6923f4
Steven Dwy <yoplitein+git@gmail.com>
2013-12-02 19:09:06 -0800

    Lock down eval.py

--- handlers/eval.py
+++ handlers/eval.py
@@ -7,12 +7,25 @@ import math, random, time
 
 args = " ".join(sys.argv[1:])
 printMode = True
+environ = {
+    "__builtins__": None,
+    "math": math,
+    "random": random,
+    "time": time,
+    "print": print,
+    "range": xrange,
+    "dir": dir,
+    "len": len,
+    "all": all,
+    "any": any,
+    "reversed": reversed,
+}
 
 if args.startswith("_="):
     printMode = False
     args = args[2:]
 
-result = eval(args)
+result = eval(args, environ, environ)
 
 if printMode:
     print (result)
