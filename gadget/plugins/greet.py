d9866873d8760e55d2daa85c4cdfc15ad8b4871d
Steven Dwy <yoplitein+git@gmail.com>
2014-01-28 14:34:28 -0800

    Reimplement greetings

---
+++ gadget/plugins/greet.py
@@ -0,0 +1,25 @@
+import random
+
+from gadget.globals import Globals
+from gadget.messages import subscribe_incoming, send_message
+
+def translate_sus(name):
+    for k,v in Globals.settings.SUS_TRANSLATIONS.iteritems():
+        if k in name:
+            return random.choice(v)
+    
+    return "sus %s" % (name,)
+
+def scan(context):
+    name = context.get("name")
+    body = context.get("body")
+    
+    if body in Globals.settings.SUS_MARKERS:
+        tmp = context.copy()
+        tmp["body"] = translate_sus(name)
+        tmp["isFormatted"] = True
+        
+        send_message(tmp)
+
+def initialize():
+    subscribe_incoming(scan)
