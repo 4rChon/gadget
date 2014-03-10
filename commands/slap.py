4dd0fc7cda87c85fe8e9beb6707bf6eeeaafd87d
4rChon <bendbug@gmail.com>
2014-03-10 20:44:49 +0100

    Added adjectives to slap.py

--- commands/slap.py
+++ commands/slap.py
@@ -6,25 +6,49 @@ import time
 
 random.seed(time.time())
 
+slapNouns = [
+    "chair leg",
+    "cliff",
+    "trout",
+    "steel",
+    "internet access",
+    "longsword",
+    "terminal",
+    "cookie",
+    "grill",
+]
+    
+slapAdjectives = [
+    "large",
+    "glowing-hot",
+    "rusty",
+    "moldy",
+    "red",
+    "old",
+    "brand spanking new",
+    "jizz stained",
+    "stupid",
+]
+
 slapMsgs = [
-    "slaps {0} around a bit with a large trout.",
-    "runs {0} over with a bulldozer.",
-    "drives {0} over a cliff.",
-    "pokes {0} with a glowing-hot rod of steel.",
-    "touches {0} with a piece of moldy bread.",
-    "revokes {0}'s internet access.",
-    "persuades {0} with a longsword.",
-    "types \"rm -rf /\" on {0}'s terminal.",
-    "eats all of {0}'s cookies.",
-    "finds {0}'s picture in the dictionary, next to the definition of \"stupid.\"",
-    "seduces {0} with a grill",
-    "touches {0} inappropriately with a chair leg",
+    "slaps {0} around a bit with a {1} {2}.",
+    "runs {0} over with a {1} {2}.",
+    "drives {0} over a {1} {2}.",
+    "pokes {0} with a {1} rod of {2}.",
+    "touches {0} with a piece of {1} {2}.",
+    "revokes {0}'s {1} {2}.",
+    "persuades {0} with a {1} {2}.",
+    "types \"rm -rf /\" on {0}'s {1} {2}.",
+    "eats {0}'s every single {1} {2}.",
+    "finds {0}'s picture in the dictionary, next to the definition of \"{1} {2}.\"",
+    "seduces {0} with a {1} {2}",
+    "touches {0} inappropriately with a {1} {2}",
 ]
 
 try:
-    target = sys.argv[1] or os.environ["name"]
+    target = sys.argv[1]
 except:
-    target = os.environ["name"]
+    target = os.environ.get("name", "goppend")
 
-print ("/me %s" % (random.choice(slapMsgs),)).format(target)
+print ("/me %s" % (random.choice(slapMsgs),)).format(target, random.choice(slapAdjectives), random.choice(slapNouns))
 
